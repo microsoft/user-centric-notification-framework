3c24351803f8f5d5122535393fce8f74606c73c4
Chinmaya Rath <chrath@microsoft.com>
2023-10-09 10:08:59 -0700

    Updated ServiceExtensions to remove template for any setup of Enterprise Email provider

--- Code/Domain/Notification.Domain.BL.SendEmail/Extensions/ServiceExtension.cs
+++ Code/Domain/Notification.Domain.BL.SendEmail/Extensions/ServiceExtension.cs
@@ -4,20 +4,11 @@
 namespace Notification.Domain.BL.SendEmail.Extensions
 {
     using Microsoft.Extensions.DependencyInjection;
-    using Notification.BL.SendEmail.Interfaces;
-    using Notification.Domain.BL.SendEmail.Internal.Helpers;
-    using Notification.Domain.BL.SendEmail.Internal.Interfaces;
-    using Notification.Domain.BL.SendEmail.Internal.Models;
 
     public static class ServiceExtension
     {
         public static void ConfigureEnterpriseEmail(this IServiceCollection services)
         {
-            services.AddSingleton<Configuration>();
-            services.AddSingleton<ITokenFactory, DefaultTokenFactory>();
-            services.AddSingleton<IEnterpriseEmailClient, EnterpriseEmailClient>();
-            services.AddSingleton<IStatusTableHelper, StatusTableHelper>();
-            services.AddScoped<IEmailNotificationHelper, EnterpriseEmailNotificationHelper>();
         }
     }
 }
